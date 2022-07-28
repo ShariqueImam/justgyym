156a9d2aadabd64cd2f1afd7f56234b99158d8d0
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-29 00:43:09 +0500

    adsf

--- components/Home/Goal.js
+++ components/Home/Goal.js
@@ -6,7 +6,7 @@ const style = {
   heading: "text-4xl md:text-4xl my-3 text-center md:text-left font-bold",
   cardContainer: "flex flex-col",
   card: "px-8 py-2 my-3 mx-3 flex items-center justify-center bg-neutral-900 hover:bg-neutral-800 cursor-pointer transition duration-[200ms] ",
-  smallHeading: "text-2xl md:text-2xl flex-1",
+  smallHeading: "text-2xl md:text-2xl flex-1 font-bold",
 };
 const Goal = (props) => {
   const handleClick = (value) => {
--- pages/level-hormone/index.js
+++ pages/level-hormone/index.js
@@ -8,42 +8,49 @@ import Animator from "../../components/UI/Animator";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
 
 const LevelHormone = (props) => {
-  props.which('home1')
+  props.which("home1");
 
   return (
     <>
-      <div className="w-[95%] md:w-[39%] lg:w-[32%] mx-auto">
+      <div className="w-[95%] md:w-[33%] lg:w-[29 %] mx-auto">
         <Animator>
           <div
             className="flex flex-col "
             style={{ fontFamily: "Inter,sans-serif" }}
           >
-            <MainHeading text={"Level of Cortisol & Testosterone"} />
+            <div className="w-[95%] md:w-[80%]">
+              <MainHeading text={"Level of Cortisol & Testosterone"} />
+            </div>
             <div className="flex mx-auto">
               <Image src="/level.webp" height={220} width={400} />
             </div>
-            <div className="flex flex-col text-gray-50 mt-8 mx-auto" style={{ fontFamily: "Inter,sans-serif" }}>
-              <h2 className="text-3xl font-bold my-4">
-                Our workout plan was created to ensure a balanced
-                testosterone/cortisol ratio.
-              </h2>
-              <p className="my-4 text-lg" style={{fontWeight:300}}>
-                Both cortisol and testosterone levels increase linearly in
-                response to physical exercise.
-              </p>
-              <p className="my-4 text-lg" style={{fontWeight:300}}>
-                However, it is of vital importance to consider them in order to
-                reach a specific threshold of exercise intensity.
-              </p>
-              <p className="my-4 text-lg" style={{fontWeight:300}}>
-                Otherwise, physical stressors can result in excessive cortisol
-                secretion, which decreases the production of testosterone.
-              </p>
-              <p className="my-4 text-lg" style={{fontWeight:300}}>
-                Overtraining and susceptibility to certain diseases and
-                disorders could be the result of an extreme stressor and a low
-                level of testosterone.
-              </p>
+            <div
+              className="flex flex-col text-gray-50 mt-8 mx-auto"
+              style={{ fontFamily: "Inter,sans-serif" }}
+            >
+              {/* <div className="w-[95%] md:w-[80%]"> */}
+                <h2 className="text-3xl font-bold my-4">
+                  Our workout plan was created to ensure a balanced
+                  testosterone/cortisol ratio.
+                </h2>
+                <p className="my-4 text-lg" style={{ fontWeight: 300 }}>
+                  Both cortisol and testosterone levels increase linearly in
+                  response to physical exercise.
+                </p>
+                <p className="my-4 text-lg" style={{ fontWeight: 300 }}>
+                  However, it is of vital importance to consider them in order
+                  to reach a specific threshold of exercise intensity.
+                </p>
+                <p className="my-4 text-lg" style={{ fontWeight: 300 }}>
+                  Otherwise, physical stressors can result in excessive cortisol
+                  secretion, which decreases the production of testosterone.
+                </p>
+                <p className="my-4 text-lg" style={{ fontWeight: 300 }}>
+                  Overtraining and susceptibility to certain diseases and
+                  disorders could be the result of an extreme stressor and a low
+                  level of testosterone.
+                </p>
+              {/* </div> */}
             </div>
             <Link href="/water">
               <div>
