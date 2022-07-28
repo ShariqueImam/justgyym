235478be3234e079b306a97736e1b2483ba82b30
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-28 21:14:49 +0500

    adsf

--- pages/attention/index.js
+++ pages/attention/index.js
@@ -9,44 +9,49 @@ const style = {
   wrapper: "flex flex-col md:flex-row",
   left: "w-[100%] md:w-[50%] flex items-center justify-center",
   right:
-    "w-[100%] md:w-[50%] bg-neutral-900 h-[60vh] md:h-[83vh] text-stone-200 flex flex-col justify-center px-4 md:px-12",
+    "w-[100%] md:w-[50%] bg-[#181818] h-[60vh] md:h-[100vh] text-stone-200 flex flex-col justify-center px-4 md:px-12",
 };
 const Attention = (props) => {
-  props.which("home1");
+  props.which("home2");
 
   return (
     <>
-      <Animator>
-        <div className={style.wrapper}  style={{fontFamily:'Inter,sans-serif'}}>
-          <div className={style.left}>
-            <Image src={"/status.webp"} width={700} height={700} />
-          </div>
-          <div className={style.right}>
+      {/* <Animator> */}
+      <div className={style.wrapper} style={{ fontFamily: "Inter,sans-serif" }}>
+        <div className={style.left}>
+          <Image src={"/status.webp"} width={800} height={950} />
+        </div>
+        <div className={style.right}>
+          <div className="w-[95%] md:w-[60%]">
             <h2
               className="text-3xl md:text-3xl lg:text-4xl my-7 text-gray-50 font-bold"
-              style={{fontFamily:'Inter,sans-serif'}}            >
+              style={{ fontFamily: "Inter,sans-serif" }}
+            >
               We care about your health
             </h2>
-            <p className="my-4">If you have any of the following conditions:</p>
+            <p className="my-4 text-lg" style={{fontWeight:300}}>If you have any of the following conditions:</p>
             <ol>
-              <li className="tracking-wide my-3">&#183; Heart disease</li>
-              <li className="tracking-wide my-3">&#183; Severe scoliosis</li>
-              <li className="tracking-wide my-3">&#183; Spinal damage</li>
-              <li className="tracking-wide my-3">&#183; Benign or malignant tumor</li>
-              <li className="tracking-wide my-3">&#183; Hypertension</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Heart disease</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Severe scoliosis</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Spinal damage</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>
+                &#183; Benign or malignant tumor
+              </li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Hypertension</li>
             </ol>
-            <p className="my-3 text-stone-300">
+            <p className="my-3 text-stone-300 text-lg" style={{fontWeight:300}}>
               Please contact your physician before starting to follow the
               MadMuscles program.
             </p>
             <Link href={"/push-ups"}>
               <div>
-                <Button text={'Continue'}/>
+                <Button text={"Got it"} />
               </div>
             </Link>
           </div>
         </div>
-      </Animator>
+      </div>
+      {/* </Animator> */}
     </>
   );
 };
