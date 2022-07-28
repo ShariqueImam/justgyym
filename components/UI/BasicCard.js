d9b91a789274b07cc7fd7eeff29b939da3b64765
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-29 00:57:51 +0500

    asdf

--- components/UI/BasicCard.js
+++ components/UI/BasicCard.js
@@ -5,7 +5,7 @@ export const BasicCard = ({ cardText, isClick }) => {
   const cardStyle = {
     card: `border-2 border-transparent ${
       isClick ? "border-orange-600" : ""
-    }  px-8 py-3 my-5 flex items-center justify-center bg-[#212121] hover:bg-[#313131] cursor-pointer transition duration-[10ms]`,
+    }  px-8 py-3 my-5 flex items-center justify-center bg-[#1E1E1E] hover:bg-[#313131] cursor-pointer transition duration-[10ms]`,
     smallHeading: "text-lg md:text-lg flex-1 text-gray-50 font-semibold",
   };
   const [Click, setClick] = React.useState(false);
