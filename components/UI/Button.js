45b75bbe7bef6b6e241ae9e722731d263c12b362
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-30 01:01:42 +0500

    asdf

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -9,8 +9,8 @@ export const Button = ({ dis, text }) => {
         : "bg-[#ff4400] cursor-pointer hover:bg-orange-600"
     }  md:max-w-fit  px-6 py-4 md:px-4 md:py-3 lg:px-4 lg:py-3 transition duration-[50ms] ml-auto my-3 md:my-6`,
     btn: `${
-      dis ? "text-stone-400" : "text-gray-100 "
-    } font-normal flex-1 md:flex-0 text-lg   tracking-wider font-bold text-left md:text-center ${
+      dis ? "text-stone-400" : "text-[#ffffff]"
+    } flex-1 md:flex-0 text-lg  tracking-wider font-bold text-left md:text-center ${
       dis ? "cursor-not-allowed" : "cursor-pointer"
     }`,
   };
