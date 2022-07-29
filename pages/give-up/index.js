3ca13d8ac704bceab7fff505334a0dc90ce70459
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-30 00:43:30 +0500

    asdf

--- pages/give-up/index.js
+++ pages/give-up/index.js
@@ -8,7 +8,7 @@ const style = {
   wrapper: "flex flex-col md:flex-row z-10",
   left: "w-[100%] md:w-[50%] flex items-center justify-center mt-24 ",
   right:
-    "w-[100%] md:w-[50%] bg-[#191919] h-[60vh] md:h-[100vh] text-gray-100 flex flex-col justify-center px-4 md:px-12",
+    "w-[100%] md:w-[50%] bg-[#191919] h-[60vh] md:h-[100vh] text-[#ffffff] flex flex-col justify-center px-4 md:px-12",
 };
 const GiveUp = (props) => {
   props.which("home2");
@@ -23,23 +23,23 @@ const GiveUp = (props) => {
         <div className={style.right}>
           <div className="w-[95%] md:w-[85%]">
             <h2
-              className="text-2xl md:text-3xl lg:text-4xl my-3 md:my-12"
-              style={{ fontFamily: "Inter,sans-serif", fontWeight: 600 }}
+              className="text-2xl md:text-3xl lg:text-4xl my-3 md:my-12 "
+              style={{ fontFamily: "Inter,sans-serif", fontWeight: 700 }}
             >
               Why do people give up on exercise?
             </h2>
-            <p className="my-4 text-lg" style={{ fontWeight: 500 }}>
+            <p className="my-4 text-lg font-thin" style={{ fontWeight: 600 }}>
               The #1 reason is starting too big too quickly
             </p>
             <p
-              className="my-3 text-gray-300 text-lg "
-              style={{ fontWeight: 300 }}
+              className="my-3 text-[#ffffff] text-lg font-bold "
+              style={{ fontWeight: 450 }}
             >
               You’ll achieve much more than just a few weeks of exercising.
             </p>
             <p
-              className="my-3 text-gray-300 text-lg"
-              style={{ fontWeight: 300 }}
+              className="my-3 text-[#ffffff] text-lg font-bold"
+              style={{ fontWeight: 450 }}
             >
               We won’t promise you quick results. Our program’s primary goal is
               to change your lifestyle for the better.
