8decdfa8e1682ab08e342c0e7414f325ad3fc38f
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-30 00:54:46 +0500

    asdf

--- pages/attention/index.js
+++ pages/attention/index.js
@@ -9,7 +9,7 @@ const style = {
   wrapper: "flex flex-col md:flex-row",
   left: "w-[100%] md:w-[50%] flex items-center justify-center",
   right:
-    "w-[100%] md:w-[50%] bg-[#181818] h-[60vh] md:h-[100vh] text-stone-200 flex flex-col justify-center px-4 md:px-12",
+    "w-[100%] md:w-[50%] bg-[#181818] h-[60vh] md:h-[100vh] text-[#ffffff] flex flex-col justify-center px-4 md:px-12",
 };
 const Attention = (props) => {
   props.which("home2");
@@ -22,24 +22,24 @@ const Attention = (props) => {
           <Image src={"/status.webp"} width={800} height={950} />
         </div>
         <div className={style.right}>
-          <div className="w-[95%] md:w-[60%]">
+          <div className="w-[95%] md:w-[85%]">
             <h2
-              className="text-3xl md:text-3xl lg:text-4xl my-7 text-gray-50 font-bold"
+              className="text-3xl md:text-3xl lg:text-4xl my-7 text-[#ffffff] font-bold"
               style={{ fontFamily: "Inter,sans-serif" }}
             >
               We care about your health
             </h2>
             <p className="my-4 text-lg" style={{fontWeight:300}}>If you have any of the following conditions:</p>
             <ol>
-              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Heart disease</li>
-              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Severe scoliosis</li>
-              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Spinal damage</li>
-              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:500}}>&#183; Heart disease</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:500}}>&#183; Severe scoliosis</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:500}}>&#183; Spinal damage</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:500}}>
                 &#183; Benign or malignant tumor
               </li>
-              <li className="tracking-wide my-3 text-lg" style={{fontWeight:300}}>&#183; Hypertension</li>
+              <li className="tracking-wide my-3 text-lg" style={{fontWeight:500}}>&#183; Hypertension</li>
             </ol>
-            <p className="my-3 text-stone-300 text-lg" style={{fontWeight:300}}>
+            <p className="my-3 text-[#ffffff] text-lg" style={{fontWeight:500}}>
               Please contact your physician before starting to follow the
               MadMuscles program.
             </p>
