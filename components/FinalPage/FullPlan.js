3e692e96fa472e5b0831cf62fa1b085a5759ad1e
Sharique Imam <shariquegithubweb@gmail.com>
2022-08-03 01:21:25 +0500

    asdf

--- components/FinalPage/FullPlan.js
+++ components/FinalPage/FullPlan.js
@@ -9,16 +9,19 @@ const FullPlan = () => {
     <div className={style.wrapper} style={{fontFamily:'Inter,sans-serif'}}>
       <h2 className="text-3xl md:text-4xl text-[#ffffff] text-center font-bold">Full Workout Plan</h2>
       <p className={style.para} style={{fontWeight:500}}>
-        <Image src={"/tick.png"} width={20} height={20} />
+        {/* <Image src={"/tick.png"} width={20} height={20} /> */}
+        <h2>☑️</h2>
         Build a habit & form correct exercising techniques
       </p>
       <p className={style.para} style={{fontWeight:500}}>
-        <Image src={"/tick.png"} width={20} height={20} />
+        {/* <Image src={"/tick.png"} width={20} height={20} /> */}
+        <h2>☑️</h2>
         Lose excessive body fat & improve workout intensity
       </p>
       <p className={style.para} style={{fontWeight:500}}>
         {" "}
-        <Image src={"/tick.png"} width={20} height={20} />
+        {/* <Image src={"/tick.png"} width={20} height={20} /> */}
+        <h2>☑️</h2>
         Achieve your goal and change your life forever
       </p>
     </div>
