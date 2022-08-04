4cec12e85ba982a09bb513e43e47ca64e7121069
Sharique Imam <shariquegithubweb@gmail.com>
2022-08-05 00:53:06 +0500

    sdf

--- pages/workout-frequency/index.js
+++ pages/workout-frequency/index.js
@@ -26,7 +26,7 @@ const WorkoutFrequency = (props) => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <>
+    <div className="mb-32 md:mb-0">
       <ProgressBar scrollLength={"64%"} val={16} link="/workout-place" />
       <div className="w-[95%] md:w-[30%] lg:w-[32%] mx-auto">
         <div>
@@ -110,7 +110,7 @@ const WorkoutFrequency = (props) => {
           )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
