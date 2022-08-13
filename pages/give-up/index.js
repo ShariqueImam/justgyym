db488b23a68c78849d7d5de88cc2af69b41b8066
Sharique Imam <shariquegithubweb@gmail.com>
2022-08-13 16:31:42 +0500

    asd

--- pages/give-up/index.js
+++ pages/give-up/index.js
@@ -8,7 +8,7 @@ const style = {
   wrapper: "flex flex-col md:flex-row z-10",
   left: "w-[100%] md:w-[50%] flex items-center justify-center mt-24",
   right:
-    "w-[100%] md:w-[50%] bg-[#191919] h-[60vh] md:h-[100vh] text-[#ffffff] flex flex-col justify-center px-4 md:px-12",
+    "w-[100%] md:w-[50%] bg-[#191919] h-[50vh] md:h-[100vh] text-[#ffffff] flex flex-col justify-center px-4 md:px-12 pb-12",
 };
 const GiveUp = (props) => {
   props.which("home2");
