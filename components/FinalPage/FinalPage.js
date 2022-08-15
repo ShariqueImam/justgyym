63399c049f7f3226e595d1767ac89544df5800c0
Sharique Imam <shariquegithubweb@gmail.com>
2022-08-15 18:48:50 +0500

    adf

--- components/FinalPage/FinalPage.js
+++ components/FinalPage/FinalPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import MoneyBack from "./MoneyBack";
 import FullPlan from "./FullPlan";
 import Plan from "./Plan/Plan";
@@ -11,12 +11,12 @@ import Video from "./Video";
 import Review from "./Review/Review";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import Popup from "./Pop";
-import { scroller } from "react-scroll";
 
 const style = {
   wrapper: "hold",
 };
 const FinalPage = () => {
+  const messagesEndRef = useRef(null);
   const [Pop, setPop] = useState(false);
   const [fat, setFat] = useState(25);
   useEffect(() => {
@@ -25,38 +25,23 @@ const FinalPage = () => {
   const handlePop = () => {
     setPop((prev) => !prev);
   };
-
-  const onScroll = (value) => {
-    if (typeof window !== "undefined") {
-      console.log(value);
-      scroller.scrollTo(`${value}`, {
-        duration: 50,
-        delay: 0,
-        smooth: "easeInOutQuart",
-      });
-    }
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
   // style={{scrollBehaviour:auto}}
   return (
-    <div
-      className={style.wrapper}
-      style={{ fontFamily: "Inter,sans-serif"}}
-    >
+    <div className={style.wrapper} style={{ fontFamily: "Inter,sans-serif" }}>
       {Pop && <Popup handlePop={handlePop} />}
-      <button
-        className="text-gray-100 bg-white px-12 py-12 text-4xl text-black"
-        onClick={() => onScroll("main")}
-      >
-        asdf
-      </button>
-      <Result fat={fat}/>
+
+      <Result fat={fat} />
       <Summary fat={fat} handlePop={handlePop} />
       <PlanFeatures fat={fat} />
       <div className="bgorange flex flex-col">
         <Get fat={fat} />
         <Video />
       </div>
-      <div className="bg1">
+      <div className="bg1" ref={messagesEndRef}>
         <FullPlan fat={fat} />
       </div>
       <div className="main flex">
@@ -64,9 +49,8 @@ const FinalPage = () => {
       </div>
       <MoneyBack fat={fat} />
       <Review fat={fat} />
-      {/* <div
-        onClick={() => onScroll("main")}
-        // style={{ zIndex: 10 }}
+      <div
+        onClick={() => scrollToBottom()} // style={{ zIndex: 10 }}
         className="fixed bottom-[0px] left-[0px] md:bottom-[80%] md:left-[78%] border-t-[2px] w-[100vw] md:w-auto md:border-none border-stone-700 bg-[#1e1e1e] md:bg-transparent cursor-pointer text-[#ffffff] font-bold flex  items-center"
       >
         <div className="flex items-center justify-center flex-1 mx-4 bg-[#ff4400] my-2 px-4 py-1">
@@ -75,7 +59,7 @@ const FinalPage = () => {
           </button>
           <AiOutlineArrowRight className={"text-2xl"} />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
--- pages/final-page/index.js
+++ pages/final-page/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import FinalPage from "../../components/FinalPage/FinalPage";
 import Footer from "../../components/Footer/Footer";
-import Animator from "../../components/UI/Animator";
 import { AiOutlineMail } from "react-icons/ai";
+
 const index = (props) => {
   props.which("home1");
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+
   return (
     <>
       {/* <a href={`${props.data[0].FileURL}?dl=latexhseet-a4.pdf`} className="text-gray-100">Download Me</a> */}
       <FinalPage />
       <Footer />
-      <div className="mb-16 md:mb-0 ">
+      <div className="mb-16 md:mb-0 main">
         <div
           className="bg-[#171717] text-[#8d8d8d] py-1 text-center font-normal text-sm"
           style={{ fontFamily: "Inter,sans-serif", fontWeight: 300 }}
